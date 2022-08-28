280fab660392e5e7ba95be36ad14b6ac329e8f44
NidhiiPandeyy <p.nidhi9797@gmail.com>
2022-08-28 12:36:58 +0530

    update

---
+++ Lab_work/exp_2.py
@@ -0,0 +1,29 @@
+# Arithmatic Operators
+
+x = 15
+y = 4
+
+print('x + y =',x+y)
+print('x - y =',x-y)
+print('x * y =',x*y)
+print('x / y =',x/y)
+print('x // y =',x//y)
+print('x ** y =',x**y)
+
+# Logical Operatorx 
+
+print(x > 10 and x < 20)
+print(x > 10 or x < 20)
+print(not(x > 10 or x < 20))
+
+# Bitwise operators
+
+a=12 
+b= 25
+print(f"a&b = {a&b}") #bitwise and
+print(f"a|b = {a|b}") #bitwise or
+print(f"a^b = {a^b}") #bitwise xor
+print(f"~35= {~35}")  #bitwise compliment
+print(f"~-12= {~-12}")
+
+
